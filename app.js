bdc14efcc45eb667274e9220c758f81be6644e56
Ubuntu <ubuntu@ip-172-31-15-148.ap-northeast-2.compute.internal>
2023-01-02 10:10:56 +0000

    fix : ec2 변경

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+// const hostname = '3.35.236.31'
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!!!");
+  res.send("Hello World!!! 수민");
 });
 
 app.listen(port, () => {
-    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+  console.log(`서버가 실행됩니다. http://localhost:${port}`);
 });
+// app.listen(port, () => console.log("Express server listening on PORT" + port));
