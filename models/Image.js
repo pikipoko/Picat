0dfb529ecde74f02cef5fb72cedcb6da80e53734
Haydn389 <ekchoi0502@gmail.com>
2023-01-03 22:54:40 +0900

    feat : db연결 처음성공

---
+++ models/Image.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const {Schema} = mongoose;//object destructing을 이용한 같은코드
+
+const imgSchema = new Schema({
+    user_id:String,
+    url:String,
+})
+
+module.exports=mongoose.model('Image',imgSchema);
