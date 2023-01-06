85e1e401b7f6622525d6cedd4e1d167a0befb1e5
pikipoko <wognsddd777@naver.com>
2023-01-06 08:58:36 +0000

    feat : kakao_signup

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ const port = 5000;
 
 /*http request 에러 방지: Origin [링크] is not allowed by Access-Control-Allow-Origin.*/
 var allowCrossDomain = function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
 };
 
 /*공통 미들웨어 장착*/
@@ -43,131 +43,131 @@ app.use(allowCrossDomain);
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    dbName: "picat", // 접속할 데이터베이스
-  })
-  .then(() => {
-    console.log("MongoDB 연결 성공");
+    .connect(process.env.MONGODB_URL, {
+        dbName: "picat", // 접속할 데이터베이스
+    })
+    .then(() => {
+        console.log("MongoDB 연결 성공");
 
-    /*라우터 구성 */
-    app.get("/test", (req, res) => {
-      const newUser = new User();
-      //값 넣어주기
-      newUser.email = "picat@abc.123";
-      newUser.name = "picat";
-      newUser.age = 25;
-      newUser
-        .save() //실제로 저장된 유저값 불러옴
-        .then((user) => {
-          console.log(user);
-          res.json({
-            message: "유저 성공적으로 생성됨",
-            email: newUser.email,
-            name: newUser.name,
-            age: newUser.age,
-          });
-        })
-        .catch((err) => {
-          res.json({
-            message: "User 생성 실패",
-          });
+        /*라우터 구성 */
+        app.get("/view", async (req, res) => {
+            const find_room = await Img.find(
+                { user_room: "room1" },
+                { _id: 0, url: 1 }
+            ).exec();
+            console.log(find_room);
+
+            res.send(find_room);
         });
-    });
-    /*find test */
-    app.get("/view", async (req, res) => {
-      // const newImg = new Img();
-      const find_room = await Img.find(
-        { user_room: "room1" },
-        { _id: 0, url: 1 }
-      ).exec();
-      console.log(find_room);
 
-      res.send(find_room);
-      // console.log(Img.find({ user_room: "room1" } ));
-      // res.send(Img.find({ user_room: "room1" }));
-    });
-
-    app.get("/", (req, res) => {
-      fs.readFile("HTMLPage.html", (error, data) => {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      });
-    });
-
-    app.post("/image", upload.single("image"), (req, res, next) => {
-      try {
-        // var file = './uploads' + req.file.filename;
-        console.log(req.file);
-        var data = req.file;
-        // res.send(data.location);
-        // res.send(data);
-        /* mongo DB에 id,url 저장하는 코드 추가 필요 */
-        //json {room:roomIdx, userid: socket.id, imgurl: data.location, }
-        const newImg = new Img();
-        //값 넣어주기
-        newImg.user_room = "room1";
-        newImg.user_id = "유저 id";
-        newImg.url = data.location;
-        newImg
-          .save() //실제로 저장된 유저값 불러옴
-          .then((user) => {
-            console.log(user);
-            res.json({
-              message: "이미지 생성정보 성공적으로 저장",
-              location: data.location,
-              user_room: newImg.user_room,
+        app.get("/", (req, res) => {
+            fs.readFile("HTMLPage.html", (error, data) => {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
             });
-          })
-          .catch((err) => {
-            res.json({
-              message: "이미지 생성정보 저장실패",
+        });
+
+        app.post("/image", upload.single("image"), (req, res, next) => {
+            try {
+                // var file = './uploads' + req.file.filename;
+                console.log(req.file);
+                var data = req.file;
+                /* mongo DB에 id,url 저장하는 코드 추가 필요 */
+                const newImg = new Img();
+                //값 넣어주기
+                newImg.user_room = "room1";
+                newImg.user_id = "유저 id";
+                newImg.url = data.location;
+                newImg
+                    .save() //실제로 저장된 유저값 불러옴
+                    .then((user) => {
+                        console.log(user);
+                        res.json({
+                            message: "이미지 생성정보 성공적으로 저장",
+                            location: data.location,
+                            user_room: newImg.user_room,
+                        });
+                    })
+                    .catch((err) => {
+                        res.json({
+                            message: "이미지 생성정보 저장실패",
+                        });
+                    });
+            } catch (error) {
+                console.error(error);
+                next(error);
+            }
+        });
+
+        app.post("/app/users/kakao", (req, res, next) => {
+            const newUser = new User();
+            const userInfo = req.body;
+
+            console.log(userInfo.nickname);
+            newUser.nickname = userInfo.nickname;
+            newUser.picture = userInfo.picture;
+            newUser.email = userInfo.email;
+            newUser.total_count = userInfo.total_count;
+            newUser.elements = [];
+            for (let i = 0; i < userInfo.total_count; i++) {
+                newUser.elements.push({
+                    id: userInfo.elements[i].id,
+                    uuid: userInfo.elements[i].uuid,
+                    favorite: userInfo.elements[i].favorite,
+                    profile_nickname: userInfo.elements[i].profile_nickname,
+                    profile_thumbnail: userInfo.elements[i].profile_thumbnail,
+                });
+            }
+
+            console.log(newUser);
+            newUser.save().then((user) => {
+                console.log(user);
+                res.json({
+                    message: "유저 성공적으로 DB 저장 및 생성됨",
+                    isSuccess: true,
+                });
             });
-          });
-      } catch (error) {
-        console.error(error);
-        next(error);
-      }
-    });
+        });
 
-    /* 소켓 통신 */
-    io.sockets.on("connection", (socket) => {
-      console.log(`Socket connected ${socket.id}`);
+        /* 소켓 통신 */
+        io.sockets.on("connection", (socket) => {
+            console.log(`Socket connected ${socket.id}`);
 
-      // message
-      var roomIdx = null;
-      socket.on("join", async (data) => {
-        // data는 브라우저에서 보낸 방 아이디
-        console.log("user joined");
-        roomIdx = data;
-        socket.join(data); // 네임스페이스 아래에 존재하는 방에 접속
-        const find_room = await Img.find(
-          { user_room: "room1" },
-          { _id: 0, url: 1 }
-        ).exec();
-        const emit_data = {
-          img_list: find_room,
-          img_cnt: find_room.length,
-        };
-        io.to(socket.id).emit("join", emit_data);
-        console.log(emit_data);
-      });
-      socket.on("image", (img_info) => {
-        // io.sockets.in(roomIdx).emit("image", img_info);
-        io.emit("image", Object.values(img_info)[0]); //모두에게 전송
-        console.log(typeof Object.values(img_info)[0]);
-        console.log(Object.values(img_info)[0]);
-      });
-      socket.on("disconnect", () => {
-        // 클라이언트의 연결이 끊어졌을 때 호출
-        console.log(`Socket disconnected : ${socket.id}`);
-      });
-    });
+            // message
+            var roomIdx = null;
+            socket.on("join", async (data) => {
+                // data는 브라우저에서 보낸 방 아이디
+                console.log("user joined");
+                roomIdx = data;
+                socket.join(data); // 네임스페이스 아래에 존재하는 방에 접속
+                const find_room = await Img.find(
+                    { user_room: "room1" },
+                    { _id: 0, url: 1 }
+                ).exec();
+                const emit_data = {
+                    img_list: find_room,
+                    img_cnt: find_room.length,
+                };
+                io.to(socket.id).emit("join", emit_data);
+                console.log(emit_data);
+            });
+            socket.on("image", (img_info) => {
+                // io.sockets.in(roomIdx).emit("image", img_info);
+                io.emit("image", Object.values(img_info)[0]); //모두에게 전송
+                // console.log(typeof Object.values(img_info)[0]);
+                // console.log(Object.values(img_info)[0]);
+            });
+            socket.on("disconnect", () => {
+                // 클라이언트의 연결이 끊어졌을 때 호출
+                console.log(`Socket disconnected : ${socket.id}`);
+            });
+        });
 
-    /*생성된 서버가 포트를 리스닝 */
-    server.listen(port, (err) => {
-      console.log(`서버가 실행됩니다. http://localhost:${port}`);
+        /*생성된 서버가 포트를 리스닝 */
+        server.listen(port, (err) => {
+            console.log(`서버가 실행됩니다. http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB 연결 에러", err);
     });
-  })
-  .catch((err) => {
-    console.error("MongoDB 연결 에러", err);
-  });
--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // const {Schema} = mongoose;//object destructing을 이용한 같은코드
 
-const userSchema = new Schema({
-    email:{
-        type:String,
-        required:true//필수사항
+const userSchema = new Schema(
+    {
+        nickname: String,
+        picture: String,
+        email: String,
+        total_count: Number,
+        elements: [
+            new Schema({
+                id: Number,
+                uuid: String,
+                favorite: Boolean,
+                profile_nickname: String,
+                profile_thumbnail_image: String,
+            }),
+        ],
     },
-    name:String,
-    age:{
-        type:Number,
-        min:18,
-        max:50
-    },
-    // enrolled:{//현재 시간을 default에 저장
-    //     type : Date,
-    //     default :Date.now 
-    // }
-},{//새로운 object 전달
-    timestamps: true//데이터 생성시간 기록, 마지막 업데이트 시간도 기록
-})
+    {
+        //새로운 object 전달
+        timestamps: true, //데이터 생성시간 기록, 마지막 업데이트 시간도 기록
+    }
+);
 
-module.exports=mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
