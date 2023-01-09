ad26a8ab0aa8bb8bed40d0f25acfbc66a681f66f
pikipoko <wognsddd777@naver.com>
2023-01-09 08:27:26 +0000

    fix. 컬럼명, 자료형 변경

--- models/Image.js
+++ models/Image.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // const {Schema} = mongoose;//object destructing을 이용한 같은코드
 
 const imgSchema = new Schema({
-    user_room : String,
-    user_id:String,
-    url:String,
-})
+  roomIdx: Number,
+  id: Number,
+  url: String,
+});
 
-module.exports=mongoose.model('Image',imgSchema);
+module.exports = mongoose.model("Image", imgSchema);
