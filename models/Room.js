4309d7ad48c62e26946c427f222cc7f34df4a6e2
pikipoko <wognsddd777@naver.com>
2023-01-14 07:09:45 +0000

    add : Room 모델 생성

---
+++ models/Room.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+  roomIdx: Number,
+  roomMemberCnt : Number,
+  members:{
+    type: Array,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model("Room", roomSchema)
