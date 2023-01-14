9196fc72875cce2e7fc501511ca626cdfab46f25
pikipoko <wognsddd777@naver.com>
2023-01-14 12:32:26 +0000

    fix : 친구 초대 - 친구 초대 호스트 방 및 친구가 기존에 있던 방 정보 수정

--- routes/friends.js
+++ routes/friends.js
@@ -1,26 +1,69 @@
 const User = require("../models/User");
+const Room = require("../models/Room");
 
 let inviteFriends = async function (req, res, next) {
   let isSuccess = true;
-  const friend_ids = req.body.friends;
-  const user = await User.findOne({ id: req.body.id }).exec();
-  const room = user.roomIdx;
-  for (let i = 0; i < friend_ids.length; i++) {
-    let pre_friend = await User.findOne({ id: friend_ids[i] }).exec();
-    if (pre_friend) {
+  const friends = req.body.friends;
+  console.log(req.body);
+
+  const host = await User.findOne({ id: req.body.id }).exec();
+  const hostRoom = await Room.findOne({ roomIdx: host.roomIdx });
+  const newMembers = hostRoom.members;
+  let memberCnt = hostRoom.roomMemberCnt;
+  for (let i = 0; i < friends.length; i++) {
+    let friend = await User.findOne({ id: friends[i] }).exec();
+    if (friend) {
       await User.updateOne(
-        { id: friend_ids[i] },
+        { id: friends[i] },
         {
           $set: {
-            roomIdx: room,
+            roomIdx: hostRoom.roomIdx,
           },
         }
-      );
-      console.log(`${pre_friend.nickname}님 - ${room} 공유방으로 이동 완료`);
+      ).then(() => {
+        console.log(`${friends[i]} 유저 업데이트 완료`);
+        if (!newMembers.includes(friend.id)) {
+          newMembers.push(friend.id);
+          memberCnt++;
+        } else
+          console.log(
+            `${friend.nickname} - 초대하려는 친구가 이미 방에 있습니다.`
+          );
+      });
+
+      // 친구가 원래 있던 방 정리
+      const friendRoom = await Room.findOne({ roomIdx: friend.id });
+      if (friendRoom) {
+        friendRoomMembers = friendRoom.members;
+        const idx = friendRoomMembers.indexOf(friend.id);
+        if (idx > -1) friendRoomMembers.splice(idx, 1);
+        await Room.updateOne(
+          { roomIdx: friend.id },
+          {
+            $set: {
+              roomMemberCnt: friendRoomMembers.length,
+              members: friendRoomMembers,
+            },
+          }
+        );
+        console.log(
+          `${friend.nickname}님 - ${hostRoom.roomIdx} 공유방으로 이동 완료`
+        );
+      }
     } else {
-      console.log(`사용자[${friend_ids[i]}]를 찾을 수 없습니다.`);
+      console.log(`사용자[${friends[i]}]를 찾을 수 없습니다.`);
     }
   }
+  // 호스트 방 업데이트
+  await Room.updateOne(
+    { roomIdx: host.roomIdx },
+    {
+      $set: {
+        members: newMembers,
+        roomMemberCnt: memberCnt,
+      },
+    }
+  );
 
   res.json({
     isSuccess: isSuccess,
