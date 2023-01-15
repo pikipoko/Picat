1149685002325875bc5c12c7d2a19c14d147fb66
pikipoko <wognsddd777@naver.com>
2023-01-15 09:05:14 +0000

    fix : 친구 초대 수정

--- routes/friends.js
+++ routes/friends.js
@@ -1,73 +1,48 @@
 const User = require("../models/User");
 const Room = require("../models/Room");
+const { checkOutTheRoom, checkInTheRoom } = require("../config/checkInOut");
 
 let inviteFriends = async function (req, res, next) {
   let isSuccess = true;
   const friends = req.body.friends;
-  console.log(req.body);
 
   const host = await User.findOne({ id: req.body.id }).exec();
-  const hostRoom = await Room.findOne({ roomIdx: host.roomIdx });
-  const newMembers = hostRoom.members;
-  let memberCnt = hostRoom.roomMemberCnt;
-  for (let i = 0; i < friends.length; i++) {
-    let friend = await User.findOne({ id: friends[i] }).exec();
-    if (friend) {
-      await User.updateOne(
+  if (host) {
+    const hostRoom = await Room.findOne({ roomIdx: host.roomIdx });
+    const newMembers = hostRoom.members;
+
+    for (let i = 0; i < friends.length; i++) {
+      await checkOutTheRoom(friends[i]);
+      await User.findOneAndUpdate(
         { id: friends[i] },
-        {
-          $set: {
-            roomIdx: hostRoom.roomIdx,
-          },
-        }
+        { $set: { roomIdx: hostRoom.roomIdx } }
       ).then(() => {
         console.log(`${friends[i]} 유저 업데이트 완료`);
-        if (!newMembers.includes(friend.id)) {
-          newMembers.push(friend.id);
-          memberCnt++;
-        } else
-          console.log(
-            `${friend.nickname} - 초대하려는 친구가 이미 방에 있습니다.`
-          );
+        if (!newMembers.includes(friends[i])) {
+          newMembers.push(friends[i]);
+        }
       });
-
-      // 친구가 원래 있던 방 정리
-      const friendRoom = await Room.findOne({ roomIdx: friend.id });
-      if (friendRoom) {
-        friendRoomMembers = friendRoom.members;
-        const idx = friendRoomMembers.indexOf(friend.id);
-        if (idx > -1) friendRoomMembers.splice(idx, 1);
-        await Room.updateOne(
-          { roomIdx: friend.id },
-          {
-            $set: {
-              roomMemberCnt: friendRoomMembers.length,
-              members: friendRoomMembers,
-            },
-          }
-        );
-        console.log(
-          `${friend.nickname}님 - ${hostRoom.roomIdx} 공유방으로 이동 완료`
-        );
+    }
+    // 호스트 방 업데이트
+    await Room.updateOne(
+      { roomIdx: host.roomIdx },
+      {
+        $set: {
+          members: newMembers,
+          roomMemberCnt: newMembers.length,
+        },
       }
-    } else {
-      console.log(`사용자[${friends[i]}]를 찾을 수 없습니다.`);
-    }
-  }
-  // 호스트 방 업데이트
-  await Room.updateOne(
-    { roomIdx: host.roomIdx },
-    {
-      $set: {
-        members: newMembers,
-        roomMemberCnt: memberCnt,
-      },
-    }
-  );
+    );
 
-  res.json({
-    isSuccess: isSuccess,
-  });
+    res.json({
+      isSuccess: isSuccess,
+    });
+  } else {
+    isSuccess = false;
+    res.json({
+      isSuccess: isSuccess,
+    });
+  }
 };
 
 module.exports = { inviteFriends };
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 require("dotenv").config;
 const fetch = require("node-fetch");
 const User = require("../models/User");
-const Room = require("../models/Room");
 const AWS = require("aws-sdk");
+
+const { checkInTheRoom } = require("../config/checkInOut");
 
 const fs = require("fs");
 const s3 = new AWS.S3({
@@ -13,21 +14,21 @@ const s3 = new AWS.S3({
   region: "ap-northeast-2",
 });
 
-const uploadImageToS3 = (imageUrl, fileName) => {
+const uploadImageToS3 = async (imageUrl, fileName) => {
   return new Promise((resolve, reject) => {
     fetch(imageUrl).then((res) => {
       res.body
         .pipe(fs.createWriteStream(`config/users/${fileName}.jpg`))
-        .on("finish", async (data) => {
+        .on("finish", (data) => {
           const param = {
             Bucket: "picat-3rd",
             Key: `users/${fileName}.jpg`, // s3 bucket 에다가 다운.
-            ACL: "public-read",
+            // ACL: "public-read",
             Body: fs.createReadStream(`config/users/${fileName}.jpg`), // 우리 서버에다가 다운
             ContentType: "image/jpg",
           };
 
-          await s3.upload(param, (error, data) => {
+          s3.upload(param, (error, data) => {
             if (error) {
               console.log("upload s3 error", error);
             }
@@ -37,24 +38,21 @@ const uploadImageToS3 = (imageUrl, fileName) => {
   });
 };
 
-let login = async function (req, res, next) {
+const login = async function (req, res, next) {
   const userInfo = req.body;
   const findUser = await User.findOne({ id: req.body.id }).exec();
-  const findRoom = await Room.findOne({ roomIdx: req.body.id }).exec();
 
   /**친구 id 저장 */
   const elements = userInfo.elements.map((obj) => obj.id);
   const friendList = userInfo.elements.map((obj) => obj.profile_nickname);
-  // for (let i = 0; i < userInfo.elements.length; i++) {
-  //   elements.push(userInfo.elements[i].id);
-  // }
+
   console.log(`카카오 친구목록 - 총${friendList.length}명 [${friendList}]`);
   console.log(`(elements) - 총${elements.length}명 [${elements}]`);
 
+  uploadImageToS3(userInfo.picture, userInfo.id);
+
   /**기존 사용자면 업데이트 */
   if (findUser) {
-    console.log(`기존 사용자 입니다 - id : ${userInfo.id}`);
-    await uploadImageToS3(userInfo.picture, userInfo.id);
     await User.updateOne(
       { id: userInfo.id },
       {
@@ -66,48 +64,25 @@ let login = async function (req, res, next) {
           elements: elements,
         },
       }
-    ).then(async () => {
-      if (!findRoom) {
-        const newRoom = new Room();
-        // 방 생성
-        newRoom.roomIdx = userInfo.id;
-        newRoom.roomMemberCnt = 1;
-        newRoom.members = [userInfo.id];
-        await newRoom
-          .save()
-          .then(() => {
-            res.json({
-              message: "유저 및 방 생성 성공",
-              isSuccess: true,
-            });
-          })
-          .catch((err) => {
-            res.json({
-              message: "방 생성 실패",
-              isSuccess: false,
-            });
-          });
-      } else {
+    )
+      .then(async () => {
         res.json({
           message: "유저 정보 업데이트 성공",
           isSuccess: true,
         });
-      }
-    });
+      })
+      .catch((err) => {
+        res.json({
+          message: "실패",
+          isSuccess: false,
+        });
+      });
   } else {
     // DB에 등록되지 않은 유저면, 즉 새로운 사용자면,
-    const newRoom = new Room();
     const newUser = new User();
-    uploadImageToS3(userInfo.picture, userInfo.id);
-
-    // 방 생성
-    newRoom.roomIdx = userInfo.id;
-    newRoom.roomMemberCnt = 1;
-    newRoom.members = [userInfo.id];
 
     // 유저 생성
     newUser.id = userInfo.id;
-    newUser.roomIdx = userInfo.id;
     newUser.nickname = userInfo.nickname;
     newUser.picture = userInfo.picture;
     newUser.email = userInfo.email;
@@ -115,47 +90,18 @@ let login = async function (req, res, next) {
     newUser.elements = elements;
 
     // 유저, 방 DB에 저장
-    await newUser
-      .save()
-      .then(async () => {
-        if (findRoom) {
-          await Room.updateOne(
-            { roomIdx: req.body.id },
-            {
-              $set: {
-                roomMemberCnt: newRoom.roomMemberCnt,
-                members: newRoom.members,
-              },
-            }
-          ).then(() => {
-            res.json({
-              message: "유저 및 방 생성 성공!",
-              isSuccess: true,
-            });
-          });
-        } else {
-          await newRoom
-            .save()
-            .then(() => {
-              res.json({
-                message: "유저 및 방 생성 성공",
-                isSuccess: true,
-              });
-            })
-            .catch((err) => {
-              res.json({
-                message: "방 생성 실패",
-                isSuccess: false,
-              });
-            });
-        }
-      })
-      .catch((err) => {
-        res.json({
-          message: "유저 생성 실패",
-          isSuccess: false,
-        });
+    await newUser.save().catch((err) => {
+      res.json({
+        message: "유저 생성 실패",
+        isSuccess: false,
       });
+    });
+    await checkInTheRoom(userInfo.id).then(() => {
+      res.json({
+        message: "새로운 유저 등록 완료",
+        isSuccess: true,
+      });
+    });
   }
 };
 
